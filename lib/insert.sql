ca40b79fb2be87fbf0d33ce078179476fa923e2b
Dakota States <dakota.states@gmail.com>
2020-03-10 00:53:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES("Mr. Chocololate", 3, "Trans", "White", "Sassy, 1");
+  INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Rowdy", 3, "Trans", "White", "Sassy, 1");
