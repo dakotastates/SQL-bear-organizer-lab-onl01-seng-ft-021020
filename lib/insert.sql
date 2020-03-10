50fa16e05c74b858d8f7385c5a4cf93725faf7be
Dakota States <dakota.states@gmail.com>
2020-03-10 01:00:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,16 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES("Mr. Chocololate", 3, "Trans", "White", "Sassy, 1");
+  VALUES("Mr. Chocololate", 3, "Trans", "White", "Sassy", 1");
   INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES("Rowdy", 3, "Trans", "White", "Sassy, 1");
+  VALUES("Rowdy", 44, "female", "blue", "gentle", 0");
+  INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Tabitha", 33, "female", "yellow", "mean", 1");
+  INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Sergeant Brown", 3, "female", "brown", "hungry", 1");
+  INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Melissa", 12, "male", "greem", "Sassy", 1");
+  INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Grinch", 10, "female", "White", "aggressive", 1");
+  INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Wendy", 473, "male", "black", "talkitive", 1");
+  INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES(NULL, 1, "male", "red", "aggressive", 0");
